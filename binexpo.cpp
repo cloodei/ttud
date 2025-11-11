d21a2027e607c18b464a3ef0211c9f53c049a78d
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-11-11 19:19:00 +0700

    init

---
+++ binexpo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+typedef __int128 i128;
+using namespace std;
+
+constexpr long long MOD = 1e9;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    long long a, b;
+    cin >> a >> b;
+
+    long long res = 1, na = a, nb = b;
+    while (nb) {
+    	if (nb & 1)
+    		res = res * na % MOD;
+
+    	na = na * na % MOD;
+    	nb >>= 1;
+    }
+
+    cout << res;
+
+    return 0;
+}
